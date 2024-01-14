007349d0875334d13ca61ad87744b1f439415a20
Furqan <furqanshaikh329@gmail.com>
2024-01-15 00:28:03 +0530

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,42 @@
+
+import { currentUser } from "@clerk/nextjs";
+import { fetchPosts } from "@/lib/actions/thread.actions"
+import ThreadCard from "@/components/cards/ThreadCard";
+import { Tabs,TabsContent,TabsList,TabsTrigger } from "@radix-ui/react-tabs"
+
+export default async function Home () {
+const user = await currentUser()
+const result = await fetchPosts(1,30)
+
+
+  return (
+    <>
+      <h1 className="head-text text-left">Home</h1>
+
+      <section className="mt-9 flex flex-col gap-10">
+        {result.posts.length===0? (
+          <p className="no-result">No Threads found</p>
+        ):(
+          <>
+          {result.posts.map((post)=>(
+            <ThreadCard 
+            key={post._id}
+            id={post._id}
+            currentUserId={user?.id || ""}
+            parentId={post.parentId}
+            content={post.text}
+            author={post.author}
+            community={post.community}
+            createdAt={post.createdAt}
+            comments={post.children}
+            
+            />
+          ))}
+          </>
+        )}
+
+     
+      </section>
+    </>
+  )
+}
---
+++ components/shared/RightSidebar.tsx
@@ -0,0 +1,17 @@
+
+
+const RightSidebar = () => {
+  return (
+    <section className="custom-scroolbar rightsidebar">
+      <div className="flex flex-1 flex-col justify-start">
+      <h3 className="text-heading4-medium text-light-1">Communities</h3>
+      </div>
+
+      <div className="flex flex-1 flex-col justify-start">
+      <h3 className="text-heading4-medium text-light-1">Sugessted Users</h3>
+      </div>
+    </section>
+  )
+}
+
+export default RightSidebar
